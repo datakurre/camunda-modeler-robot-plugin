7a70112bb9cd72713823003bb13101f44a7bc67c
Asko Soukka <asko.soukka@iki.fi>
2022-12-24 20:56:09 +0200

    Add module build exporting 'RobotTaskModule'

--- module.rollup.config.js
+++ module.rollup.config.js
@@ -4,7 +4,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
 
-export default {
+export default [{
   input: "client/bpmn-js-extension/index.js",
   output: {
     file: "dist/module.js"
@@ -19,4 +19,21 @@ export default {
       "process.env.NODE_ENV": JSON.stringify("production")
     })
   ]
-};
+}, {
+  input: "client/bpmn-js-extension/index.js",
+  output: {
+    format: "iife",
+    file: "dist/module-iife.js",
+    name: "RobotTaskModule"
+  },
+  plugins: [
+    resolve(),
+    babel({"presets": ["@babel/preset-env"], "babelHelpers": 'bundled'}),
+    commonjs({
+      include: "node_modules/**"
+    }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    })
+  ]
+}];
